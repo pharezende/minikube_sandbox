b986fbb544e43017531b8483b880e6dc4d2cd5a0
Pedro <phamorimrezende@gmail.com>
2022-12-24 16:04:20 -0300

    chore: use ingress instead of external service

--- build.sh
+++ build.sh
@@ -15,15 +15,17 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 # Instantiation
 cd $namespace_path
 minikube start
+minikube addons enable ingress
 alias kubectl="minikube kubectl --"
 kubectl apply -f mongo-namespace.yaml
 kubectl apply -f mongo-secret.yaml 
 kubectl apply -f mongo.yaml 
 kubectl apply -f mongo-configmap.yaml 
 kubectl apply -f mongo-express.yaml 
+kubectl apply -f mongo-express-ingress.yaml
 kubens dev # Can be installed by 'snap install kubectx'
 echo "Please wait..."
-sleep 40 # It takes some time for the cluster to go online
-minikube service mongo-express-service -n $environment
+sleep 60 # It takes some time for the cluster to go online
+xdg-open http://mymongoexpress.com/ # (need to map in /etc/hosts file)
 
 # Run as . build.sh
